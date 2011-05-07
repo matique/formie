50fe8a210ce566ad572b6fe651ca3ac3d54fcec7
matique <dittmar.krall@matique.de>
2011-05-07 18:34:13 +0200

    some cleaning; Rails3

--- init.rb
+++ init.rb
@@ -1,18 +1,24 @@
-# Include hook code here
-
 ActionView::Base.send :include, Formie::ActionView
 
 
 # inject formies from default directories
 
+def define_formie(where, name, txt)
+  txt = txt.force_encoding('UTF-8')  if txt.respond_to?(:force_encoding)
+  where.define_formie name, :inline => txt
+end
+
 def inject(where, dir)
-  Dir[File.join(dir, '*.html.erb')].each {|formie| where.define formie }
+  Dir[File.join(dir, '*.html.erb')].each {|filename|
+    formie = File.basename(filename, '.html.erb')
+    define_formie(where, formie, File.open(filename).read)
+  }
   Dir[File.join(dir, '*.yml')].each {|yml|
     formies = YAML.load(File.open(yml).read)
-    formies.each {|formie, txt| where.define formie, :inline => txt}
+    formies.each {|formie, txt|  define_formie(where, formie, txt) }
   }
 end
 
-root = "#{RAILS_ROOT}/app/views/formie"
+root = "#{::Rails.root.to_s}/app/views/formie"
 inject Formie::FormBuilder, "#{root}/forms"
 inject Formie::ActionView,  "#{root}/templates"
--- lib/formie.rb
+++ lib/formie.rb
@@ -2,44 +2,34 @@ module Formie
 
   module Helpers
 
-    def self.define_formie(where, form, name, *args)
-      root = "#{RAILS_ROOT}/app/views/formie"
-      formiename = File.basename(name.to_s, '.html.erb').to_sym
-      filename   = name
-      filename   = File.join(root, "#{name}.html.erb") if formiename == name
-
+    def self.define_formie(where, name, *args)
+      formiename = name
       options  = args.extract_options!
-      if (template = options.delete(:template))
-	filename = File.join(root, "_#{template}.html.erb")
-      end
-
-      unless inline = options.delete(:inline)
-	inline = File.open(filename, "rb") {|f| f.read}
-	inline ||= "formie '#{formiename}' missed"
-      end
+      inline = options.delete(:inline)
 
       where.send(:define_method, formiename, lambda {|*args|
-#p "** Lambda ** #{where} #{formiename}", args
+#p ["** Lambda ** #{where} #{formiename}", args]
 	params = args.extract_options!
 	options = {}
-	options.update :inline => inline
+	options[:inline] = inline
 	options[:locals] = {}
 	options[:locals].update params
-	options[:locals].update :form => self
-	options[:locals].update :formiename => formiename
-
-	(@template && @template.render(options)) || controller.render(options)
+	options[:locals].update :formiename => formiename,
+			:inline => inline, :form => self,
+			:args => args
+	(@template && @template.render(options)) ||
+		controller._render_template(options)
       })
-#p "formie #{where} #{formiename} defined"
+#p ["** #{where} #{formiename} defined"]
     end
   end
 
 
   class FormBuilder < ActionView::Helpers::FormBuilder
 
-    def self.define(name, *args)
-#p "** define Formie #{name} ", args
-      Helpers::define_formie(self, self, name, *args)
+    def self.define_formie(name, *args)
+#p ["*** define Formie #{name} ", args]
+      Helpers::define_formie(self, name, *args)
     end
   end
 
@@ -52,8 +42,7 @@ module Formie
 		*args, &proc)
       options = args.extract_options!
       # hand control over to the original #form_for()
-##      form_for(record_or_name_or_array,
-      fields_for(record_or_name_or_array,
+      form_for(record_or_name_or_array,
 		*(args << options.merge!(:builder => builder)), &proc)
     end
 
@@ -63,9 +52,9 @@ module Formie
 		Formie::FormBuilder, *args, &proc)
     end
 
-    def self.define(name, *args)
-#p "** define Formie #{name} ", args
-      Helpers::define_formie(self, nil, name, *args)
+    def self.define_formie(name, *args)
+#p ["** define Formie #{name} ", args]
+      Helpers::define_formie(self, name, *args)
     end
   end
 
