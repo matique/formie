e808e46cf4d9a3bf52b48835ad78195b39cc4614
matique <dittmar.krall@matique.de>
2011-05-12 17:22:23 +0200

    block added

--- lib/formie.rb
+++ lib/formie.rb
@@ -7,14 +7,15 @@ module Formie
 #p ["** defining #{where}.#{name}"]
       formiename = name
 
-      where.send(:define_method, formiename, lambda {|*args|
-#p ["** called #{where} #{formiename}", args]
+      where.send(:define_method, formiename, lambda {|*args, &block|
+#p ["** called #{where} #{formiename}", args, block]
 	params = args.extract_options!
 	options = {}
 	options[:inline] = txt
 	options[:locals] = {}
 	options[:locals].update params
 	options[:locals].update :formiename => formiename,
+			:block => block,
 			:form => self, :args => args
 	(@template && @template.render(options)) ||
 		controller._render_template(options)
