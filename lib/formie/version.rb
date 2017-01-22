bf3f50e983265dea41d15966c1396ea2b303fee3
matique <dittmar.krall@matique.de>
2017-01-22 10:04:38 +0100

    'v0.9.3; missing initializer; .gitignore'

--- lib/formie/version.rb
+++ lib/formie/version.rb
@@ -1,3 +1,3 @@
 module Formie
-  VERSION = '0.9.2'
+  VERSION = '0.9.3'
 end
--- spec/internal/app/controllers/application_controller.rb
+++ spec/internal/app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
 
-=begin
-  if Rails.version < '5.0.0'
-    before_filter do
-      Formie.reload  unless Rails.env.production?
-    end
-  else
-    before_action do
-      Formie.reload  unless Rails.env.production?
-    end
-  end
-=end
-
 end
