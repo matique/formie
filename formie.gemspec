8ed9c406e1b829573fa4b15b5b79a7035674ffc3
matique <dittmar.krall@matique.de>
2017-08-10 18:38:36 +0200

    'rollback'

--- formie.gemspec
+++ formie.gemspec
@@ -28,7 +28,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'slim'
 
   s.add_development_dependency 'capybara'
-  s.add_development_dependency 'combustion', '~> 0.7.0'
+  s.add_development_dependency 'combustion'
   s.add_development_dependency 'minitest'
   s.add_development_dependency 'simplecov'
   s.add_development_dependency 'sqlite3'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,14 +6,8 @@ end
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require 'bundler'
-Bundler.require :default, :development
-
 require 'combustion'
-#Combustion.initialize! :active_record, :action_view
-#Combustion.initialize! :active_record, :action_view, :action_controller
-#Combustion.initialize! :active_record, :action_controller
-Combustion.initialize! :all
+Combustion.initialize! :active_record, :action_controller
 
 require "rails/test_help"
 require 'minitest/autorun'
