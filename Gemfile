0e028da8c8c51c4840e756c7276972643b3d5f4e
matique <dittmar.krall@matique.de>
2018-04-27 15:54:20 +0200

    '...'

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ gemspec
 
 gem 'rails', '~> 5.2.0'
 gem 'dryer-config', '~> 5.2.0'
-gem 'activestorage'
 
 group :test do
   gem 'observr'
--- test/internal/app/controllers/application_controller.rb
+++ test/internal/app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-require 'active_storage'
-
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
