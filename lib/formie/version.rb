0f9c439915ce851fe6ad3fe7dfce003d2c62b4de
matique <dittmar.krall@matique.de>
2020-07-14 19:57:57 +0200

    'v1.0.2'

--- lib/formie/version.rb
+++ lib/formie/version.rb
@@ -1,7 +1,8 @@
 # rubocop: disable all
 
 module Formie
-  VERSION = '1.0.1'     # 2020-04-27
+  VERSION = '1.0.2'     # 2020-07-14
+# VERSION = '1.0.1'     # 2020-04-27
 # VERSION = '1.0.0'     # 2019-09-26
 # VERSION = '0.9.16'    # 2019-03-05
 # VERSION = '0.9.15'    # 2019-03-02
